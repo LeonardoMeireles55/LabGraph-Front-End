0a8fc96e80349c4e4456e79f3b31f626a524d15d
Leonardo <leomeireles55@hotmail.com>
2024-12-12 11:24:13 -0300

    fix: correct date formatting in formatDateWithTime utility to avoid off-by-one error

--- src/components/functional/formatDateWithTime.tsx
+++ src/components/functional/formatDateWithTime.tsx
@@ -1,7 +1,7 @@
 const formatToTwoDigits = (value: number) => String(value).padStart(2, '0');
 
 const formatDateWithTime = (year: number, month: number, day: number) => {
-    const formattedDate = `${year}-${formatToTwoDigits(month)}-${formatToTwoDigits(day + 1)}`;
+    const formattedDate = `${year}-${formatToTwoDigits(month)}-${formatToTwoDigits(day)}`;
     return `${formattedDate} 00:00:00`;
 };
 

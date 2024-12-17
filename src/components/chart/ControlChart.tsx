c0d6fa4a13d05534994da5d4d781bae20e89df0a
leonardomeireles55 <leomeireles55@hotmail.com>
2024-12-17 13:38:47 -0300

    fix: increase animation duration in ControlChart component from 400ms to 500ms

--- src/components/chart/ControlChart.tsx
+++ src/components/chart/ControlChart.tsx
@@ -147,7 +147,7 @@ const ControlChart: React.FC<ControlChartProps> = ({ listing }) => {
                                 strokeWidth={1.2}
                                 activeDot={{ r: 4 }}
                                 dot={<CustomDot />}
-                                animationDuration={400}
+                                animationDuration={500}
                             />
 
                             {yAxisValues.map((line, index) => (

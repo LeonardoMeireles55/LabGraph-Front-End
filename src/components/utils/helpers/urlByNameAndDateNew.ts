9529647162c77b521a13bc3557424a4684e3dd8e
leonardomeireles55 <leomeireles55@hotmail.com>
2024-12-29 16:06:19 -0300

    fix: correct URL construction in urlByNameAndDateNew helper function

--- src/components/utils/helpers/urlByNameAndDateNew.ts
+++ src/components/utils/helpers/urlByNameAndDateNew.ts
@@ -9,7 +9,7 @@ const urlByNameAndDateNew = (props: UrlAnalyticsByNameAndDatePropsNew) => {
     const startDate = formatDateWithTime(date.startYear, date.startMonth, date.startDay);
     const endDate = formatDateWithTime(date.endYear, date.endMonth, date.endDay);
 
-    const url = (`${process.env.NEXT_PUBLIC_API_BASE_URL}/${analyticsType}${process.env.NEXT_PUBLIC_API_BASE_URL_RESULTS_GROUPED}${testFormatFix(name)}&startDate=${startDate}&endDate=${endDate}`);
+    const url = (`${process.env.NEXT_PUBLIC_API_BASE_URL}/${analyticsType}/${process.env.NEXT_PUBLIC_API_BASE_URL_RESULTS_GROUPED}${testFormatFix(name)}&startDate=${startDate}&endDate=${endDate}`);
 
     return { url };
 };

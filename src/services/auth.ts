01d04eb47aab2a4f7a1078b2c303b78bebc415d6
leonardomeireles55 <leomeireles55@hotmail.com>
2025-01-04 14:23:16 -0300

    refactor: update authentication API endpoints and improve response handling

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,24 +1,25 @@
 import { AuthFormData } from '@/components/auth/types/Auth';
+import checkResponse from '@/components/utils/helpers/checkResponse';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const authService = {
   async signIn(credentials: Pick<AuthFormData, 'email' | 'password'>) {
-    const response = await fetch(`${API_BASE_URL}/user/signIn`, {
+    const response = await fetch(`${API_BASE_URL}/users/sign-in`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(credentials),
     });
-    return await response.json();
+    return await checkResponse(response);
   },
 
   async signUp(userData: Omit<AuthFormData, 'confirmPassword'>) {
-    const response = await fetch(`${API_BASE_URL}/user/signUp`, {
+    const response = await fetch(`${API_BASE_URL}/users/sign-up`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(userData),
     });
-    return await response.json();
+    return await checkResponse(response);
   },
 
   async setSession(token: string) {

668d3b24ab1bd6e04c13e19c71d4cf16d5ea1d02
leonardomeireles55 <leomeireles55@hotmail.com>
2025-01-20 12:34:10 -0300

    feat: update loading message to English for better accessibility

--- src/components/utils/components/loading/index.tsx
+++ src/components/utils/components/loading/index.tsx
@@ -15,7 +15,7 @@ const Loading: React.FC = () => {
     <div className='flex flex-col items-center justify-center gap-4'>
       <div className='spinner border-blue-500 h-12 w-12 animate-spin rounded-full border-t-4 text-primary' />
       {showMessage && (
-        <p className='text-gray-600 text-sm text-textPrimary'>Tente selecionar outra data.</p>
+        <p className='text-gray-600 text-sm text-textPrimary'>Try select other date range.</p>
       )}
     </div>
   );

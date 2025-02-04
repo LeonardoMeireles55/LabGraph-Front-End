2e51dd5696a285ca3231d522e722b31ec1302304
Leonardo <leomeireles55@hotmail.com>
2025-02-04 20:59:37 -0300

    hotfix: remove trailing slash configuration from Next.js settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: process.env.NODE_ENV === 'development',
-  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },

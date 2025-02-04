11cf8934a68a5a04e7b90169af2de750e454503d
Leonardo <leomeireles55@hotmail.com>
2025-02-04 19:19:55 -0300

    fix: enable trailing slash configuration in Next.js settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: process.env.NODE_ENV === 'development',
+  trailingSlash: false,
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
 };
 
 module.exports = nextConfig;
